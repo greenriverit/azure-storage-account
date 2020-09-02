374c5abae19863710acc8cb9d4f777c5a0048d4c
AgileCloudInstitute <64223536+AgileCloudInstitute@users.noreply.github.com>
2020-09-02 16:04:49 -0700

    Create variables.tf

---
+++ modules/terraform-azure-storage-account/variables.tf
@@ -0,0 +1,23 @@
+## Copyright 2020 Green River IT (GreenRiverIT.com) as described in LICENSE.txt distributed with this project on GitHub.  
+## Start at https://github.com/AgileCloudInstitute?tab=repositories    
+
+####################################################################################################################
+####Define the input variables:  
+####################################################################################################################
+#Note that the client referred to is an App Registration.
+variable "subscriptionId" { }
+variable "tenantId" { }
+variable "clientId" { }
+variable "clientSecret" { }
+variable "storageAccountNameTerraformBackend" { }
+variable "storageContainerNameTerraformBackend" { }
+variable "resourceGroupName" { }
+variable "resourceGroupLocation" { }
+variable "subnetId" { }
+variable "environmentName" { }
+
+##Define the output variables
+data "azurerm_subscription" "current" {}
+output "subscription_name" { value = data.azurerm_subscription.current.display_name }
+output "subscription_id" { value = data.azurerm_subscription.current.subscription_id }
+output "tenant_id" { value = data.azurerm_subscription.current.tenant_id }
